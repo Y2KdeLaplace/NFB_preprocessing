ca3b83d79e9a77517f6a1b0b34a880d91e147afc
Yiheng Hu <77317792+Y2KdeLaplace@users.noreply.github.com>
2026-04-27 16:18:31 +0800

    Add files via upload

---
+++ makeup_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf nii_files/*
+mv -f pyOpenNFT-setting/NF_Data_* nii_files/
+mv -f pyOpenNFT-setting/ROIs/* nii_files/
+mv -f pyOpenNFT-setting/settings/* nii_files/
+mv -f pyOpenNFT-setting/weights/* nii_files/
+mv -f pyOpenNFT-setting/*.* nii_files/
+
+echo -n "Please input subject ID (like hyh_sub-00 or sub-00):"
+read -r subj
+today=$(date +%Y%m%d)
+mv nii_files /mnt/d/Data_RT/$today_$subj
+mkdir nii_files
